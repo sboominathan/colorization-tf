ae3eb94c62b2c1eb092d5c53c30357b3828a402d
Kevin Weng <kweng@mit.edu>
2017-12-10 15:19:04 -0500

    added resnet model and added model arg to training

--- rgb_tuner.py
+++ rgb_tuner.py
@@ -1,5 +1,5 @@
 from keras.models import Model
-from keras.layers import Input
+from keras.layers import Input, Add
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D, Conv2DTranspose
 
@@ -7,7 +7,7 @@ from keras.layers.convolutional import Conv2D, Conv2DTranspose
 def conv_downsample_block(x, block_id, filter, block, end_stride):
 
     for i in range(1, block + 1):
-        name = 'conv_block_{0}_{1}'.format(block_id, i)
+        name = 'conv_block_{}_{}'.format(block_id, i)
         stride = end_stride if i == block else 1
 
         kwargs = {'padding': 'same',
@@ -22,7 +22,7 @@ def conv_downsample_block(x, block_id, filter, block, end_stride):
 
 def conv_upsample_block(x, block_id, filter, block, end_stride):
     for i in range(1, block + 1):
-        name = 'conv_block_{0}_{1}'.format(block_id, i)
+        name = 'conv_block_{}_{}'.format(block_id, i)
         stride = end_stride if i == block else 1
 
         kwargs = {'padding': 'same',
@@ -35,7 +35,7 @@ def conv_upsample_block(x, block_id, filter, block, end_stride):
     return x
 
 
-def rgb_tuner(img_dim, batch_size, model_name='colorize'):
+def rgb_tuner(img_dim, model_name='tuner_cnn'):
     h, w, c = img_dim
     curr_h, curr_w = img_dim[:2]
     block_id = 1
@@ -61,5 +61,41 @@ def rgb_tuner(img_dim, batch_size, model_name='colorize'):
         block_id += 1
 
     x = Conv2D(3, 2, strides=1, padding='same')(x)
+
+    return Model(inputs=[inputs], outputs=[x], name=model_name)
+
+
+def residual_block(x, block_id, filter):
+    name = 'block_{}_conv_a'.format(block_id)
+    r = Conv2D(filter, 3, padding="same", activation='relu', name=name)(x)
+    r = BatchNormalization(axis=1,
+                           name='block_{}_bn_a'.format(block_id))(r)
+
+    name = 'block_{}_conv_b'.format(block_id)
+    r = Conv2D(filter, 3, padding="same", activation='relu', name=name)(r)
+    r = BatchNormalization(axis=1,
+                           name='block_{}_bn_b'.format(block_id))(r)
+
+    x = Add(name='block_{}_merge'.format(block_id))([x, r])
+
+    return x
+
+
+def rgb_tuner_resnet(img_dim, model_name='tuner_resnet'):
+    h, w, c = img_dim
+    block_id = 1
+    n_blocks = 3
+
+    inputs = Input(shape=(h, w, c), name="input")
+    name = 'block_{}_initial'.format(block_id)
+    x = Conv2D(64, 3, padding='same',
+               activation='relu', name=name)(inputs)
+
+    for i, filter in enumerate([64] * n_blocks):
+        x = residual_block(x, block_id, filter)
+        block_id += 1
+
+    x = Conv2D(3, 2, strides=1, padding='same',
+               name='block_{}_final'.format(block_id))(x)
 
     return Model(inputs=[inputs], outputs=[x], name=model_name)
--- train_tuner.py
+++ train_tuner.py
@@ -2,62 +2,46 @@ import os
 import glob
 import sys
 import numpy as np
-from skimage.io import imread, imsave
+from skimage.io import imread
 import keras.backend as K
-from rgb_tuner import rgb_tuner
+from rgb_tuner import rgb_tuner, rgb_tuner_resnet
 from keras.callbacks import ModelCheckpoint
 import argparse
 
 
 parser = argparse.ArgumentParser(sys.argv[0])
-parser.add_argument('--train_x', type=str)
-parser.add_argument('--train_y', type=str)
+parser.add_argument('--input', type=str)
+parser.add_argument('--target', type=str)
+parser.add_argument('--model', type=str, default='cnn')
 
 args = parser.parse_args()
 
 
 def main():
     global args
+    assert args.model in ['cnn', 'resnet']
     img_dim = [128, 128, 3]
     batch_size = 50
     n_epochs = 50
 
-    # data_dir = 'data'
-    # train_dir = os.path.join(data_dir, 'train')
-    # val_dir = os.path.join(data_dir, 'val')
-    # test_dir = os.path.join(data_dir, 'test')
+    input, target = get_data(args.input, args.target)
 
-    train_x, train_y = get_data(args.train_x, args.train_y)
-    # val = get_data(val_dir)
-    # test = get_data(test_dir)
-
-    model = rgb_tuner(img_dim, batch_size)
+    if args.model == 'cnn':
+        model = rgb_tuner(img_dim)
+    else:
+        model = rgb_tuner_resnet(img_dim)
     print model.summary()
+
     model.compile(loss=rgb_loss, optimizer='adam')
-    # model.fit(x=train_x, y=train_y, validation_data=val,
-    #           batch_size=batch_size, epochs=n_epochs)
     print 'Model created and compiled.'
 
-    model_save = 'rgb_tuner.best.hdf5'
+    model_save = 'rgb_tuner.{}.best.hdf5'.format(args.model)
     checkpoint = ModelCheckpoint(model_save,
                                  monitor='val_loss',
                                  save_best_only=True)
 
-    model.fit(x=train_x, y=train_y, validation_split=0.1,
+    model.fit(x=input, y=target, validation_split=0.1,
               batch_size=batch_size, epochs=n_epochs, callbacks=[checkpoint])
-
-    # preds = model.predict(test[0], verbose=1)
-
-    # pred_path = os.path.join(test_dir, 'pred')
-    # if not os.path.exists(pred_path):
-    #     os.makedirs(pred_path)
-
-    # assert preds[0].shape == tuple(img_dim)
-
-    # for i, pred in preds:
-    #     img_path = os.path.join(pred_path,
-    #                             '{}_pred.jpg'.format(str(i + 1).zfill(8)))
-    #     imsave(img_path, pred)
 
 
 def rgb_loss(img_true, img_pred):
